65e113fd309999e60b82ec40315e12f1397502c4
simon t <satabligan@ucdavis.edu>
2019-12-22 19:59:32 -0800

    Added ground and objects

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
