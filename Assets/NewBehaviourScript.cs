b32c13f9b12af136d4ae85777a802aa0c2ca3328
simon t <satabligan@ucdavis.edu>
2019-12-22 22:35:21 -0800

    Ball motion control

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -5,9 +5,24 @@ public class NewBehaviourScript : MonoBehaviour
 
     public Rigidbody rb;
 
+    public float forwardForce = 1000f;
+    public float sidewaysForce = 600f;
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        // rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if ( Input.GetKey("w") ) {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+
+        if ( Input.GetKey("a") ) {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if ( Input.GetKey("d") ) {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
